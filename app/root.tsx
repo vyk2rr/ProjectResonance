8932ba9e2d6d049e3342721a14a25fc7a9947577
Victor De la Rocha <vyk2rr@gmail.com>
2025-05-28 15:24:44 -0600

    Adds examples ensuring Radix UI is installed and working

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import '@radix-ui/themes/styles.css';
+
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
